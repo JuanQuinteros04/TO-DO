667fe1dcaa60855a85db52ab2c36dcb8e2d0fa42
JuanQuinteros04 <juanmanuelquinteros@gmail.com>
2023-05-30 17:48:15 -0300

    First commit

---
+++ src/test/java/com/quinteros/rest/RestFulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinteros.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
